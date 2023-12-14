1e87d6771c23f9ea8b7f840b716adb3e3bac4b0b
Martin Arusalu <martin.arusalu@gmail.com>
2023-12-14 10:25:33 +0200

    Day 14 solution

---
+++ day14.ts
@@ -0,0 +1,272 @@
+const real =
+  `##..O..#...#..#..#...#.......O.#.#...#..#...#....#.#...##..OO....#.O.....O.O.#...O..#.........#.#OO.
+O#O.OOO......OO...O##.#.........#OOO..#...#....#.O...#.#.OO......O..#.O#....#..#.O.....O...O......#.
+O.O....O..#.O#.O#......#O.#.#O.#.....OO.#O.......O.#O.#...#.O###.OO..O...O..O#...#....#.#.O....O.O..
+O.....#.....#.....O##..OO..O.##..O....O..O...O.........O.....#.#.OO.OO....O..#O....#...O.OO.O...O..O
+.O.....#..O..#.........O#...........O.#..#..#.OO#..#....O.#O..OO..O#O.O#.##..#...OO#..O...#...O.....
+......O...O.OO......##.O....#....O#...#.......#..#..##.###....OO.O#.O..#.O..OO#.O#O..#..#.O..O.....#
+O..O##...O#..#.##.....O##......O..O......##.#...O...#........O.#.....O#O...#O...O...#...O..O.O#.##..
+....#.#.....OO#O.#.O...#OO...#..##.#...O...O.#...#.O.O#.....OO.....O.#.O#..##.O#.##O..O..#O.#.#.#...
+O.#...............###...OO.O...O..OO.#...........####...###OO#.O##O..#.#.....O....#OO.O...#O......OO
+O.....O........O.OOO....#......OO..##.OO.#OO#..#.O....#.O.##...#..#.##......O..O..O#.....#...#O...#.
+O....O..O..#.OOO...#....O#........OO#O..O.....O#..#...#.#..O...#......O#........OO#...O......O..###O
+OOO..OO.................O##OO..#O..O#.#..O....OO...O..OO.#.#.O#OO....#..##....O....O....#..##.....O.
+O.....O.O#O.##.......O..O.O##.....#O..O#...O..O..........O.#........#O.#O.#.#OO..O#.......#.#...O#O.
+.#.....O.OO...O####O..O...#.O.##.#.O.O.#..#..OO.O..##....#OOO...O..#.O#.O#.....OOO.O#O#.#O...O#O.#..
+.OO.O.#........OO.#O...O..#....O.#...........O...O.#..O.....#.O.O..O#.......O.O#O.###..#O..O....O#..
+...O..#.OOO..#..OO.OOO#..OOO.O.#O...O.O#..#....O...O....#.....O..O.##.#OO.#.#....#....###..#O#...#..
+..##O#.#..O#.O....OO.#O.O.OOOO.##..OO...OO..O......O.O..O#..O.#....O.#O...O.OOOO#.O..#OO...O..#.....
+...#..O....O.....#.OO.#O......O......#..O#...O#...#OO...O#......#.O.O...#OOO.#..O........O#....#..#.
+O.........O.......##.O.O#...#.#....#.#..#....#.OO...O#.#O.#O...O...#O.OO...O.#....#OO.O..#.###.O..O.
+.#....#.#..#.O..#..OO.#.....O...O...#O..O.#......#...O....##......#.#O..O.##......#...##.O..O..OO.#.
+........O.O#O..#...OO.O...O.....O.O.##.O....##.###O#O..O..#.##...O..##.......O.##..............#..#O
+##.....O....O.###..#O..#....OO..OO..OO..O.....O..#....OO....#..O#.....#O..#.#O.#......O#.........#.O
+....O..O#.#.O..O.O...OO...........#...O.#.OOOO..OO..##........O......#...O.......#.........OO...#.O#
+O..OO..#.O...##.......#.......O...##.O.#......#....O.#...O....#...#..#..O...O..OO..##O...O##.OO#....
+OOOOO..#...#OO.........#....O..#.#OO..........O.....O..O.#.......#.OO..#.#.O.#....O..O.....O.#..OO#.
+O.OO.#O.#........O#.#O.O...#.......#O.O....#.#.O.#O...#...#O#.O#...#.#..O..O#OO#.O.O.......O.#O.....
+....#.O.........OOO...#..O...#..O..#O.#OO...O.##O...#..O...##......O...O..O.#..#O..O..#.#...O..O.O..
+O.......O....#..O.O....#......#..#......##..#.#..O...#..O..O.O..#.#O.#OO.O#.....O##O..O.#..###O..O..
+O.O.O..........#O.OO.O....O..#..O##..O..OO....O.OO.#O.........O..#......O........OO....#O.........O.
+#...#.O..###.........O.OO.#....#OO....#.O.......O..OO.O.#O#..##.O..O......O.....#..#.#..O.O#.#OO.O.#
+....O....#..O..OO......OO#..##OO.O.#O.O#...O..#O#......O.....OO....OOO.#.OOO.O##O........O....O.....
+..OO#..O...#.O.#OO.....#..O...O....#...OO...#O.#...........O..##....O.OOO.OO#O.....#.........#.....O
+.O...O..#...#O...O..OOO.#..O..OO#..#.##..OO.#.OO.O.....#O......O..#.#..#....#.OO..#....#...OO...O.OO
+...#......O.#.O.O....O.....O#..O#..#...#.#...#O........#.#OOO.........O....#.O##OOO.....#.#O#....O..
+.O..#..OO..O#.#..O.O...O.O#..O..O..O...OOOO.O....O..##...#OO##.#..O##O.#......#.O...OO#O##..#.#.#O.O
+..#........#.#..O.OO.#O....#.#..##..#.......#O..#...O.#....#........O...O.##.OO#.......#.OO.##O#.O#.
+#O..#....O###OO.#.#.##....O...#..#O.#.#......#.....##.#O#O..O..#..#.OO.O..##...O...........#.....#O#
+OO....###.O.#O.#...#..#O.O.....O...#O#.O#..O..#O#......O.O...O.#....O.#.#O..OO...O.#...O..O...O.###.
+O..O.O..OO.OO.#O..#............#......O...#....O###.O...#.O.O....#...OOO...O.O......O..##.....O.##.O
+.O.O..OO.#O...#.#O#..O#O...##.#.##...O...O.O.O..OO.OO.O..#.O.#.O.........O.O#.O.#.#......O.....#...#
+#.##.......#O.....#......OOO...O.........#O.O.#O#..O.....O...O#.#.....#..#........####.O.O..O..##O..
+.O.............OO.O..#..#.OOO...#.O..##O..O...O..O..OO............O.O.O.OO..O.#O..#OOO.O..##..OOO#.#
+.#.......O.....O..O..OO.....O..#O...OO#O.....#..#..OO..#.O.O.#........O......O.....O...O...OO.O.....
+#.#..#.#.O..O..............O#O#...O.O..#.#.O.#.#.....#..O#.O...#........O.#..O....#O##OO..O...##...O
+.#....#.#..O.O...O..O#...O..O..##....O......O#..O.O...OO.O.#...O.......#..O....O....##.#...OO.#.....
+..##....#.O.#..O.#......O#...###.#.##O.O..#........O..O#O.........#.###O.O#...O..#OOO.##O.#.O....#.#
+##..#...O....O...OO.OO..O#..#.#.O.O.......O#.O.O........O..##...O..#......O..#..O.O......##...OO....
+O..#..O.......#O.O..##.O.O#....OO#.O.....#......##...#..###.OO..#..O.#...OO..OO.OO.OO.OO......#....#
+.O...#OOO.O...#.O#......OO..O....#...O.OO#......#.....O#O....#....##O#.......#O#O.##O....#....O.O...
+...#O.#O...O..O..O..#..OO.O#..........#.OO..O.....#O.OO.O....O.O...O..#............O#..O..O..#O#.#..
+....O.O...O........O..##..O.O..#..#.O...O#OO#..O..#..#OO#O..O...#.O#.##...OO...#...#......O..#...O.O
+O.....#O#....O#...#.O..OO.#.#....#O#.O..#...#..O##O..#.O..O..O#.O..OO.#.#O..O....#...OOO.....O....#.
+.#.#..O.##.O#.........O#.O....O....#.#...#O..O....O...#..O#.#..O....O.....OO.....#......OO.....#..O.
+O......O.....#.....O##O#O......#....OO...#..O...O....#O......OO.O##.O#O.OOO.....OO.#...O#...O...#..#
+O..O...O#...#...O..#..O..O...#O..#O.....O##.#.#...O.O...#..O...#.OO#O..#..#.#.O.O.O..#..O#....O#O...
+.........#O.O....OO.#OOO.....O#...O..#.O#O#.#........O...#O.....O.#.#.O.O#...........O...#...#.....#
+..O...OO#O.#O..OO..OO###.OO....O......###..O........OO..#..O..........#.......O..O..O..#...O..OO..#.
+#.OO...........OO#.....O......O##..O.#....O.O.#....OO.#O##OO...O...O..O.#.#OO....#.#......#.##.O...O
+..#..#.O.....#..#..O#..O..O......##OO#O....#.......OOO...#O..O...#..O...#..O......O.O..O..O.#...O#..
+.O.OO#...#.......#.......O..O#.O.....#.O..O..OO#......###..OO.O.O.#.#........#.#.#........OO#O.#...O
+.##....#..OO..O.OO.O.#...O#.##.#...O....O.#.....#...#..O#.##.....O....###.O.#...O.#.O..#.#......O...
+OO..O..#.O...###.O..#..O.OO#..O.....O.O...O.....##OOO.O#.O.....#..#.....O....O#...#..O.O#.#.....O...
+...##.O..#...O.#..#OO.....O#....##OO.....OO....#O...................#.O#OO.#.OO................O.O..
+...O...#.....O..#.#.O..O.O......O.........O.O#..#.........##......#..#....O..#..##O.....O.O......O.#
+.#...O........#.OOO......#OO.O#........#.....#.#.....#OO.#.O.O..O.O..#...O...#..###.....OOO...O#OO..
+...OOO....O...#..#.O......O..#.##O.#.OO...OOO#O...##O#.....#...#..O#....#.....#O#..O#..#...#..OO....
+.O...#..OO..O...#...O..##.....#O##..O#...O.O..O##..OOO#.#..#O..#...#..OO.....#....##.#.......O#.O..O
+O#OO..O#O..........#..#......#..O#.#O#...O...#...OO..........OO..OOO#O..O.O.....O.O.#O.O....#.......
+#O#...O#O..O...O.....OO..........#OOOO.#O..OO#O#O..#.O.#.#O##..#.O.OO.#...#.....#O..O....O...#...O#.
+O.#.O.OOO..O.O.O##OO...O##..O...###..OOO...O..#..#.......#O.........#.#....OO.#.O#...#.O...O.O.....O
+#.#..O..O.O###..#O.....#......OO.O..##.#......#.O#OO.....O......#.........O#...OO..#.....O...O.#.O..
+..O........O...##..#....#.......O.##OOO........O....O......#....#..O#...O..........O##....#.#O.O#O#O
+..##O#.O.........O.O...........O.OO.#..##.O#.#.OO..O##O.....#O...#..O...#...#.O...#...##.......O.O..
+.O..#.....#O.O.#O#O#.....O....#....##O....#.......OO.#...O#...OO#...O..#.O#...O.O#..O.O##O.....OO.#.
+.O.O......#.O...O.#.#O....OO.....O.#..OOO#.O..#O.#OO....##OO.OOOOO#.OO...#.O...#......#.O......#.#OO
+.#O.OO.#O.....#.O..O#...........O#OO.#.....#.....#.OO......O.#.#.#O..O#.#O.O.#O......OOO.#..O.OO.O..
+O........OOO..OO#.#.##.#...#.O.......#....O.##O...##O.O.#..#..O.O......#...#..#.#O....#...OO........
+O..#.#....#.#.O..O#OOO....O..O....O#.OO#.#.O.O......OO..#O..O#O.##....O.##....#O.O#.O...........O.O.
+...O.#.#.O..O#....#.O..O.O...O..O.#..O#...O#......O.#...O.OOO.##OOO...#....O.....#..O....O.O.#..#...
+.O..O#.#.O.....#O..#O.O.#....OO..#......#.#..........O#..##...O...O#...O.OO..O#O.....O.OO.O#O..O....
+#...O...OO.#.OO.OO..O..#.#.O....##..O..O....#OO#.....O.O.............##..........O....O..#.O.##.....
+....#....##O.#..O#.#.#.O.#O....#O..O##O...#.#...OO#O.##.#......#O.OO...#.......#O#...O#.......O.#..O
+....O.##.O.O#.#....O.....O#O.O.O#.O.........OOO#.#O##O..O.O#..OO.#.#O.......##O...O.....O...O....O..
+....O....#..OO...#.O.#..O......O..O..##.O#..#OO..OO.O....O##....O.##.#......O.OO##....O....#.O#.....
+#O..O#..O..#..OO.......O...O.....O...O..O.#.#.#OO.O...........#..#..O..O#.#.#O.......#O.#..#.#.O.O..
+.O....#.##.#...O....O.#OOOO.O#O.##O#..##..#O.#..#.O.#O......#...#......O.OO.O.O#..O#.O.O...##..#O...
+.O...O.......###.#...##.#..O..O...#.#....#OOOOO.O.#O#....O..O#.O#O...#.OO...O.....OO.O.O.O..#.O#....
+........O#..#O.#..O.#..###O#...O#..#.O..#OO...#.#.#.O#......O....O......#..##..#OO.O#OOO.#..#.O..#O.
+#..#O....O..O.....#O.#O..O.O#.#.O.......#O.OO.#.O...O.O...O.O..#.#O##O..#O.#.OO...#........O.O#...#.
+...#..O..##..O.#..#..O.#.#.O....OOO#...O.##.#.O.#...O#..OO.OOO#....O..O....OO#O.#O.#...O..#.O...O.#.
+.O....#OO#.#O......#O..OO...#O.OO.......O..O##O#.#..OO.O.#O#..........#OO...............OO.#...#O..#
+.###..##...O..O.O....O...#.O..OO.#.#O.#.....O...O..#.....#..........#O.O#.#..O....##O..OO.O...#.O.O.
+..#...OO#.....#......##....O...O..O..O.#.O..#.O##.##..OO.O...#...O....O..#..O##.#..#.OO....#O#......
+.....##......OO.O..#......#.#.O##..#.O.........O.OO..O.#....O.....#O..O.....#O.....#O........#..O...
+....#..O..OOO..OO...O.##.O..........#O.....#O.O.OOOOOOO...OO.....O......#..OOO#...####O..OO.O.#.....
+.O...O#.OOOOO##O#.....#...O.O.#..#.....O.#......O..O.OO..OO.#.OO...#O#O..#.O..O....OO.O..O..O.#O....
+.......OO...#......#.O.......#.OO..#..O##O#.#.O.O.#........O.....#O....O..#....OOO...OOO..........O.
+O..O..#...#.#.O#...O.....O.#.....O..#O#.....#....#.O..O.#O.#..#....O.....OO..O..#...O.....O#.O...#..
+#O....O.#OOO#.........O...O#...O##O..O#O.O.....O...O.#.O..##...#OO..#..O..#...#....#.#.....#....OO..
+#OO.....#..#...OO.....O...#...............#.......#.OOOO#.#.....O.O.O.#.O..##......OO#..#...O......O`;
+const test = `O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....`;
+
+function format(input: string) {
+  return input.split("\n").map((line) => line.split(""));
+}
+
+function part1(input: string) {
+  console.time("part1");
+  let result = 0;
+
+  const data = format(input);
+  const rolled = [...data.map((e) => [...e])];
+
+  for (let row = 0; row < data.length; row++) {
+    for (let col = 0; col < data[row].length; col++) {
+      if (data[row][col] === "O") {
+        // roll
+        let final = row;
+        for (let k = row; k > 0; k--) {
+          if (
+            k > 0 && ["#", "O"].includes(rolled[k - 1][col])
+          ) {
+            final = k;
+            break;
+          }
+
+          if (k === 1 && rolled[0][col] === ".") {
+            final = 0;
+          }
+        }
+
+        rolled[row][col] = ".";
+        rolled[final][col] = "O";
+
+        result += rolled.length - final;
+      }
+    }
+  }
+
+  console.log(result);
+  console.timeEnd("part1");
+}
+
+function part2(input: string) {
+  console.time("part2");
+  let result = 0;
+
+  const data = format(input).map((l) => l.join(""));
+
+  function roll(original: string[]) {
+    const rolled = original;
+
+    for (let row = 0; row < original.length; row++) {
+      for (let col = 0; col < original[row].length; col++) {
+        if (original[row].charAt(col) === "O") {
+          // roll
+          let final = col;
+          const before = rolled[row].slice(0, col);
+          const blocks = [...before.matchAll(/(#|O)/g)];
+          if (blocks.length > 0) {
+            const lastBlock = blocks[blocks.length - 1];
+            final = (lastBlock.index ?? 0) + 1;
+          } else {
+            final = 0;
+          }
+
+          if (final !== col) {
+            rolled[row] = rolled[row].substring(0, final) + "O" +
+              rolled[row].substring(final + 1);
+            rolled[row] = rolled[row].substring(0, col) + "." +
+              rolled[row].substring(col + 1);
+          }
+        }
+      }
+    }
+
+    return rolled;
+  }
+
+  function doCycle(original: string[]) {
+    // north
+    let rolled = original;
+    const toNorth = [];
+    for (let col = 0; col < rolled[0].length; col++) {
+      let line = "";
+      for (let row = 0; row < rolled.length; row++) {
+        line += rolled[row].charAt(col);
+      }
+      toNorth.push(line);
+    }
+    rolled = roll(toNorth);
+    // west
+    const toWest = [];
+    for (let col = 0; col < rolled[0].length; col++) {
+      let line = "";
+      for (let row = 0; row < rolled.length; row++) {
+        line += rolled[row].charAt(col);
+      }
+      toWest.push(line);
+    }
+    rolled = roll(toWest);
+    // south
+    const toSouth = [];
+    for (let col = 0; col < rolled[0].length; col++) {
+      let line = "";
+      for (let row = rolled.length - 1; row >= 0; row--) {
+        line += rolled[row].charAt(col);
+      }
+      toSouth.push(line);
+    }
+    rolled = roll(toSouth);
+    // east
+    const toEast = [];
+    for (let col = rolled[0].length - 1; col >= 0; col--) {
+      let line = "";
+      for (let row = rolled.length - 1; row >= 0; row--) {
+        line += rolled[row].charAt(col);
+      }
+      toEast.push(line);
+    }
+    rolled = roll(toEast);
+
+    // back to original direction
+    const toOriginal = rolled.map((r) => r.split("").reverse().join(""));
+    return toOriginal;
+  }
+
+  const totalCycles = 1000000000;
+  let rolled = data;
+  const positions: string[] = [rolled.join(";")];
+  let inLoop = false;
+  for (let cycle = 1; cycle <= totalCycles; cycle++) {
+    rolled = doCycle(rolled);
+
+    if (!inLoop) {
+      const loopStartPos = positions.indexOf(rolled.join(";"));
+      if (loopStartPos >= 0) {
+        // we in a loop now
+        inLoop = true;
+        cycle = totalCycles - ((totalCycles - cycle) % (cycle - loopStartPos));
+      } else {
+        positions.push(rolled.join(";"));
+      }
+    }
+  }
+
+  for (let row = 0; row < rolled.length; row++) {
+    for (let col = 0; col < rolled[row].length; col++) {
+      if (rolled[row].charAt(col) === "O") {
+        result += rolled.length - row;
+      }
+    }
+  }
+
+  console.log(result);
+  console.timeEnd("part2");
+}
+
+part1(test);
+part1(real);
+part2(test);
+part2(real);
